5819547edc9984e3679e564676a92de926dce65f
Lokesh Kumar <lokesh.kumar05@nagarro.com>
2022-09-27 11:05:13 +0530

    sql file added

---
+++ sql_queries.sql
@@ -0,0 +1,3 @@
+select rb_hbe_mortgage_application.STATUS from rb_home_finance.rb_hbe_mortgage_application where user_id = 'mohittest01'
+
+UPDATE rb_home_finance.rb_hbe_mortgage_application SET STATUS = 'POA_EPN_REVIEW_COMPLETED' where user_id = 'mohittest01'
